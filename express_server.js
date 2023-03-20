36364db4cdb61889087a0347f0064f0dd4146e18
Kevin Cho <kevcho604@gmail.com>
2023-03-20 16:31:00 -0700

    Added ejs index

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+}); 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -24,3 +29,4 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
