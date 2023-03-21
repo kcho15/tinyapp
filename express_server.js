b645cd618eef8b94152ba858968121c913abfa4c
Kevin Cho <kevcho604@gmail.com>
2023-03-20 19:47:43 -0700

    Added Post method to new url page

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080; // default port 8080
   
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,6 +16,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 }); 
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+ 
 app.get("/urls/:id", (req, res) => {
   const IDs = req.params.id;
   const templateVars = { id: IDs, longURL: urlDatabase[IDs] }; // assigned req.params.id to a variable IDs 
